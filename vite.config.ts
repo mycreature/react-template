6d7af988fb870a577926a53781243fdcc1337fca
Minseop Kim <aistjq1691@gmail.com>
2025-04-03 15:14:29 +0900

    "디폴트 프로젝트 설정"

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),    tailwindcss(),],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],  // '@'를 '/src' 폴더로 대체
+  },
+})
